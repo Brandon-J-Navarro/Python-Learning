50819f3d8ec43d4295edf859b235b7c8b1031208
Brandon <111472416+Brandon-J-Navarro@users.noreply.github.com>
2025-02-04 15:10:42 -0500

    inital commit

--- Learning02/__main__.py
+++ Learning02/__main__.py
@@ -1,6 +1,19 @@
 # __main__.py
+from datetime import datetime
 
-def myFunction() -> None:
-    print('Hello World!')
+def my_printName_function(user_name) -> None:
+    print('Hello', user_name, '!')
+    print(user_name,'Python is WEIRD!\n')
 
-myFunction()
+def my_printAge_function(user_birthYear) -> None:
+    date = datetime.now()
+    user_age = date.year - user_birthYear
+    print('You are', user_age, 'old!\n')
+    for i in range(user_birthYear, date.year + 1):
+        print("You have lived through years:", i)
+
+if __name__ == "__main__":
+    user_name = input('Enter your name: ')
+    my_printName_function(user_name)
+    user_birthYear = int(input("Enter you Birth year: "))
+    my_printAge_function(user_birthYear)
