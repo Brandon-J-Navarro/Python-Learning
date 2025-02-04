01d39c892ef237b70f4476b4fab983bee1733713
Brandon <111472416+Brandon-J-Navarro@users.noreply.github.com>
2025-02-04 14:17:50 -0500

    initial commit

---
+++ Learning01/learning01.py
@@ -0,0 +1,8 @@
+i = 0
+while True:
+    print('Hello World!')
+    i = i + 1
+    if i > 10:
+        print('Goodbye World!')
+        break
+i = int(input('Input a Number\n'))
---
+++ Learning02/__main__.py
@@ -0,0 +1,6 @@
+# __main__.py
+
+def myFunction() -> None:
+    print('Hello World!')
+
+myFunction()
